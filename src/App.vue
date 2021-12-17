1f35665ebdbf01a766a2249407d5f32699f7f1fb
longfuyang <lx812833@163.com>
2021-12-17 17:15:51 +0800

    Echart图表适配以及业务分装

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,34 @@
   <div id="app" ref="app">
     <div class="left_container">
       <div class="left_top">
-        1
-        <!-- <line-chart-page /> -->
+        <line-chart-page />
       </div>
       <div class="left_center">
-        2
-        <!-- <line-chart-page /> -->
+        <line-chart-page />
       </div>
       <div class="left_bottom">
-        3
-        <!-- <line-chart-page /> -->
+        <line-chart-page />
       </div>
     </div>
 
     <div class="center_container">
       <div class="center_top">
-        4
-        <!-- <Map /> -->
+        <Map />
       </div>
       <div class="center_bottom">
-        5
-        <!-- <bar-chart-page /> -->
+        <bar-chart-page />
       </div>
     </div>
 
     <div class="right_container">
       <div class="right_top">
-        6
-        <!-- <line-chart-page /> -->
+        <line-chart-page />
       </div>
       <div class="right_center">
-        7
-        <!-- <line-chart-page /> -->
+        <line-chart-page />
       </div>
       <div class="right_bottom">
-        8
-        <!-- <line-chart-page /> -->
+        <line-chart-page />
       </div>
     </div>
   </div>
@@ -45,11 +37,16 @@
 
 <script>
 import styleUtil from "@/utils/styleUtil.js";
+import BarChartPage from "@/views/BarChartPage";
+import LineChartPage from "@/views/LineChartPage";
+import Map from "@/components/MapChart";
 
 export default {
   name: "App",
   components: {
-    // HelloWorld,
+    BarChartPage,
+    LineChartPage,
+    Map
   },
   methods: {
     // 动态新增DOM结构
@@ -67,30 +64,82 @@ export default {
 
       // (窗口宽度 / 窗口高度) < (设计稿宽度 / 设计稿高度) ? (窗口宽度 / 设计稿高度) : (窗口高度 / 设计稿高度)
       const scale = document.documentElement.clientWidth / document.documentElement.clientHeight < designWidth / designHeight
-        ? document.documentElement.clientWidth / designWidth
-        : document.documentElement.clientHeight / designHeight;
+          ? document.documentElement.clientWidth / designWidth
+          : document.documentElement.clientHeight / designHeight;
       document.querySelector("#app").style.transform = `scale(${scale})`;
     },
+    // 基于rem实现等比缩放
+    handleSetFontSize() {
+      const designWidth = 1920; 
+      const designHeight = 1080; 
+
+      let fontSize = document.documentElement.clientWidth / document.documentElement.clientHeight < designWidth / designHeight
+          ? (document.documentElement.clientWidth / designWidth) * 12
+          : (document.documentElement.clientHeight / designHeight) * 12;
+      document.querySelector("#app").style.fontSize = fontSize + "px";
+
+      // div {
+      //   width: px2rem(500);
+      //   height: px2rem(500);
+      // }
+    }
+
   },
   mounted() {
     // this.handleDOMDynamic();
-    this.handleSetScale();
-    window.addEventListener("resize", this.handleSetScale);
+    // this.handleSetScale();
+    // window.addEventListener("resize", this.handleSetScale);
+
+    // this.handleSetFontSize();
   },
   beforeDestroy() {
-    window.removeEventListener("resize", this.handleSetScale);
-  }
+    // window.removeEventListener("resize", this.handleSetScale);
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 #app {
+  display: flex;
   width: 100vw;
   height: 100vh;
-  /* padding-top: vh(60); */
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  background: #ccc;
+  padding: vh(20);
+  box-sizing: border-box;
+  .left_container,
+  .right_container {
+    > div {
+      min-width: 250px;
+      width: vw(450);
+      height: vh(330);
+      background: rgb(202, 202, 202);
+      border-radius: 10px;
+    }
+
+    .left_center,
+    .right_center {
+      margin: vh(20) 0;
+    }
+  }
+
+  .center_container {
+    // 需要设置一个最小宽高，否则再绘图的时候，发现宽高为0的话会引起Echarts报错
+    min-width: 600px;
+    width: vw(1200);
+    height: vh(500);
+    margin: 0 vh(20);
+
+    .center_top {
+      height: vh(680);
+      background: rgb(202, 202, 202);
+      border-radius: vh(10);
+    }
+
+    .center_bottom {
+      height: vh(330);
+      margin-top: vh(20);
+      background: rgb(202, 202, 202);
+      border-radius: vh(10);
+    }
+  }
 }
 </style>
