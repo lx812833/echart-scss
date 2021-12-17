82f565e6bf176a2bc52a9e5ed9f2d88445ab82b9
longfuyang <lx812833@163.com>
2021-12-17 13:50:55 +0800

    等比缩放适配---基于transform的适配方案

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,55 @@
 <template>
   <div id="app" ref="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="left_container">
+      <div class="left_top">
+        1
+        <!-- <line-chart-page /> -->
+      </div>
+      <div class="left_center">
+        2
+        <!-- <line-chart-page /> -->
+      </div>
+      <div class="left_bottom">
+        3
+        <!-- <line-chart-page /> -->
+      </div>
+    </div>
+
+    <div class="center_container">
+      <div class="center_top">
+        4
+        <!-- <Map /> -->
+      </div>
+      <div class="center_bottom">
+        5
+        <!-- <bar-chart-page /> -->
+      </div>
+    </div>
+
+    <div class="right_container">
+      <div class="right_top">
+        6
+        <!-- <line-chart-page /> -->
+      </div>
+      <div class="right_center">
+        7
+        <!-- <line-chart-page /> -->
+      </div>
+      <div class="right_bottom">
+        8
+        <!-- <line-chart-page /> -->
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import styleUtil from "@/utils/styleUtil.js";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
   methods: {
     // 动态新增DOM结构
@@ -23,20 +60,37 @@ export default {
       dom.style.height = styleUtil.px2vh(300);
       dom.style.background = "#ccc";
     },
+    // 基于transform的适配方案
+    handleSetScale() {
+      const designWidth = 1920; // 设计稿的宽度
+      const designHeight = 1080; // 设计稿的高度
+
+      // (窗口宽度 / 窗口高度) < (设计稿宽度 / 设计稿高度) ? (窗口宽度 / 设计稿高度) : (窗口高度 / 设计稿高度)
+      const scale = document.documentElement.clientWidth / document.documentElement.clientHeight < designWidth / designHeight
+        ? document.documentElement.clientWidth / designWidth
+        : document.documentElement.clientHeight / designHeight;
+      document.querySelector("#app").style.transform = `scale(${scale})`;
+    },
   },
   mounted() {
-    this.handleDOMDynamic();
+    // this.handleDOMDynamic();
+    this.handleSetScale();
+    window.addEventListener("resize", this.handleSetScale);
   },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.handleSetScale);
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 #app {
+  width: 100vw;
+  height: 100vh;
+  /* padding-top: vh(60); */
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: vh(60);
+  background: #ccc;
 }
 </style>
